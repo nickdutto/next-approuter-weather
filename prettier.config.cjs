7ec0d3b81aa3480361aa34c044efd2030dc3051f
Nick <delta2888@gmail.com>
2023-06-30 13:42:48 +1000

    Initial Commit

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindFunctions: ['clsx'],
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+};
+
+module.exports = config;
