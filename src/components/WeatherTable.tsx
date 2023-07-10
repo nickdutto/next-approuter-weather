9b6c0e654744982cb9ba4e2128774b2b4a486598
Nick <delta2888@gmail.com>
2023-07-10 18:19:12 +1000

    feat: add WeatherTable component

---
+++ src/components/WeatherTable.tsx
@@ -0,0 +1,64 @@
+import { parseISO } from 'date-fns';
+
+import { type SunriseSunset, type Weather } from '~/app/page';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/ui/table';
+import { getWeatherIcon } from '~/utils/weather-utils';
+
+type Props = {
+  weather: Weather;
+  sunriseSunset: SunriseSunset;
+};
+
+const WeatherTable = ({ weather, sunriseSunset }: Props) => {
+  return (
+    <Table className="rounded-md bg-zinc-900">
+      <TableCaption>Weather</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Date</TableHead>
+          <TableHead>Time</TableHead>
+          <TableHead>Type</TableHead>
+          <TableHead>Temperature</TableHead>
+          <TableHead>Humidity</TableHead>
+          <TableHead>Pressure</TableHead>
+          <TableHead>Wind Speed</TableHead>
+          <TableHead>Wind Gust</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {weather.data.timelines[0].intervals.map((interval) => (
+          <TableRow key={interval.startTime} className="p-2">
+            <TableCell>{parseISO(interval.startTime).toLocaleDateString()}</TableCell>
+            <TableCell>{parseISO(interval.startTime).toLocaleTimeString()}</TableCell>
+            <TableCell>
+              <img
+                src={`./tio/${getWeatherIcon(
+                  interval.startTime,
+                  sunriseSunset.results.sunrise,
+                  sunriseSunset.results.sunset,
+                  interval.values.weatherCode,
+                )}`}
+                alt=""
+              />
+            </TableCell>
+            <TableCell>{interval.values.temperature}</TableCell>
+            <TableCell>{interval.values.humidity}</TableCell>
+            <TableCell>{interval.values.pressureSeaLevel}</TableCell>
+            <TableCell>{interval.values.windSpeed}</TableCell>
+            <TableCell>{interval.values.windGust}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default WeatherTable;
