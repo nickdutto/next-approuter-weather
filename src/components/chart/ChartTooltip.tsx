d7a956218ac96696fc042dc45c2d933113585cd0
Nick <delta2888@gmail.com>
2023-07-11 17:27:08 +1000

    feat: update ChartTooltip styling

--- src/components/chart/ChartTooltip.tsx
+++ src/components/chart/ChartTooltip.tsx
@@ -11,9 +11,12 @@ const ChartTooltip = ({ point }: Props) => {
   const formattedDate = format(date, 'EEEE - dd/MM/yy: HH:mm');
 
   return (
-    <div className="flex flex-col items-center rounded-md bg-zinc-800 p-2">
-      <div className="mb-2">{formattedDate}</div>
-      <div>{point.data.yFormatted}hPa</div>
+    <div className="flex flex-col items-center rounded-md bg-zinc-900 p-2">
+      <div className="mb-2 text-xs text-zinc-400">{formattedDate}</div>
+      <div className="text-zinc-200">
+        {Number(point.data.yFormatted).toFixed(1)}
+        <span className="text-xs text-zinc-400">hPa</span>
+      </div>
     </div>
   );
 };
