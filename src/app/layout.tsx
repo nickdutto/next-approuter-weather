10fdd3db6ed5409105d0c0ea2ce80010c9b22bc3
Nick <delta2888@gmail.com>
2023-07-11 14:59:00 +1000

    feat: add MainNav to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 
+import MainNav from '~/components/layouts/MainNav';
 import { ThemeProvider } from '~/components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <MainNav />
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
