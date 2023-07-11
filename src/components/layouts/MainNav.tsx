163dbc4911b27bb18bb994717dd2d352c2c98254
Nick <delta2888@gmail.com>
2023-07-11 14:58:34 +1000

    feat: add MainNav component (Navbar)

---
+++ src/components/layouts/MainNav.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '~/components/ui/navigation-menu';
+import { cn } from '~/lib/utils';
+
+const MainNav = () => {
+  const path = usePathname();
+
+  return (
+    <div className="relative flex items-center justify-center bg-black p-2">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  path === '/' && 'dark:bg-blue-500 dark:hover:bg-blue-500 dark:focus:bg-blue-500',
+                )}
+              >
+                Dashboard
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/hourly" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  path === '/hourly' &&
+                    'dark:bg-blue-500 dark:hover:bg-blue-500 dark:focus:bg-blue-500',
+                )}
+              >
+                Hourly
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/charts" legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  path === '/charts' &&
+                    'dark:bg-blue-500 dark:hover:bg-blue-500 dark:focus:bg-blue-500',
+                )}
+              >
+                Charts
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+
+export default MainNav;
