5eeb0f79f794c292f110de1a851fcd9f9cfda891
Nick <delta2888@gmail.com>
2023-07-11 16:30:17 +1000

    feat: convert Icon to react-icons from lucide

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,9 +1,9 @@
-import { LucideIcon, LucideProps } from 'lucide-react';
+import { type IconType, type IconBaseProps } from 'react-icons';
 
 type IconProps = {
-  icon: LucideIcon;
+  icon: IconType;
   size?: string | number;
-} & LucideProps;
+} & IconBaseProps;
 
 const Icon = ({ icon: IconComponent, size = 20, ...restProps }: IconProps) => {
   return <IconComponent size={size} {...restProps} />;
