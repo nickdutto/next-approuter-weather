69cf48e98c23b20746ae19479cebd90624ad495d
Nick <delta2888@gmail.com>
2023-07-11 16:30:48 +1000

    feat: add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Inter } from 'next/font/google';
+import Footer from '~/components/layouts/Footer';
 
 import MainNav from '~/components/layouts/MainNav';
 import { ThemeProvider } from '~/components/ThemeProvider';
@@ -16,9 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <MainNav />
         <ThemeProvider attribute="class" defaultTheme="dark">
+          <MainNav />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+const Footer = () => {
+  return (
+    <footer className="relative w-full bg-black p-4">
+      <a href="https://www.tomorrow.io/weather-api" target="_blank">
+        <img
+          src="./tio/logo/tomorrowio-logo.svg"
+          alt="Tomorrow.io Logo"
+          className="absolute bottom-1 right-2 w-1/5"
+        />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
