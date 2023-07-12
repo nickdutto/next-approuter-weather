03d54f82634dbe181aa3d8ac69c6d7e9a2dff71e
Nick <delta2888@gmail.com>
2023-07-12 14:48:33 +1000

    feat: add RiverTable
    - RiverTable displays: date,time, watercourse discharge, water level

---
+++ src/components/river/RiverTable.tsx
@@ -0,0 +1,46 @@
+import { formatInTimeZone } from 'date-fns-tz';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/ui/table';
+import { type MergedRiverData } from '~/utils/river-utils';
+
+type Props = {
+  riverData: MergedRiverData;
+};
+
+const RiverTable = ({ riverData }: Props) => {
+  return (
+    <Table className="rounded-md bg-zinc-950">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-center">Date</TableHead>
+          <TableHead className="text-center">Time</TableHead>
+          <TableHead className="text-center">Discharge (cumec)</TableHead>
+          <TableHead className="text-center">Level (m)</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="text-gray-300">
+        {riverData.map((data) => (
+          <TableRow key={data.date}>
+            <TableCell className="text-center text-xs">
+              {formatInTimeZone(data.date, 'Australia/Canberra', 'dd/MM/yy')}
+            </TableCell>
+            <TableCell className="text-center text-xs">
+              {formatInTimeZone(data.date, 'Australia/Canberra', 'HH:mm')}
+            </TableCell>
+            <TableCell className="text-center text-xs">{data.discharge}</TableCell>
+            <TableCell className="text-center text-xs">{data.level}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default RiverTable;
