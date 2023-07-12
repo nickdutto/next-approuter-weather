db2b8bfd350ad7611cc39f651a22a844be98ece9
Nick <delta2888@gmail.com>
2023-07-12 12:13:38 +1000

    feat: add yScale slider to ChartContainer
    - Slider sets the yScale padding between 0 & 100 in 5 step intervals

--- src/components/chart/ChartContainer.tsx
+++ src/components/chart/ChartContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { type Serie } from '@nivo/line';
+import { Slider } from '~/components/ui/slider';
 
 import { useEffect, useMemo, useState } from 'react';
 
@@ -14,12 +15,14 @@ import {
 } from '~/components/ui/select';
 import { type Weather } from '~/types/types';
 import { createChartData, getMinMaxValues } from '~/utils/weather-utils';
+import { Label } from '~/components/ui/label';
 
 type Props = {
   weather: Weather;
 };
 
 const ChartContainer = ({ weather }: Props) => {
+  const [sliderValue, setSliderValue] = useState([10]);
   const [selectValue, setSelectValue] = useState('pressureSeaLevel');
   const [chartData, setChartData] = useState<Serie[] | null>(null);
   const [minMaxY, setMinMaxY] = useState({ min: 0, max: 0 });
@@ -52,16 +55,27 @@ const ChartContainer = ({ weather }: Props) => {
 
   return (
     <div className="relative w-full">
-      <div className="flex justify-end pr-5">
-        <Select onValueChange={setSelectValue} defaultValue={selectValue}>
-          <SelectTrigger className="w-[200px]">
+      <div className="flex items-center justify-end gap-2 bg-zinc-950 pr-5 pt-3">
+        <div className="flex w-full p-2">
+          <Label htmlFor="minmax" className="min-w-fit px-2">
+            yScale: {sliderValue[0]}
+          </Label>
+          <Slider
+            id="minmax"
+            defaultValue={sliderValue}
+            onValueChange={setSliderValue}
+            max={100}
+            step={5}
+          />
+        </div>
+        <Select defaultValue={selectValue} onValueChange={setSelectValue}>
+          <SelectTrigger className="w-[140px]">
             <SelectValue placeholder="Weather Field" />
           </SelectTrigger>
           <SelectContent>
             <SelectItem value="temperature">Temperature</SelectItem>
-            <SelectItem value="temperatureApparent">Temperature Apparent</SelectItem>
             <SelectItem value="humidity">Humidity</SelectItem>
-            <SelectItem value="pressureSeaLevel">Pressure Sea Level</SelectItem>
+            <SelectItem value="pressureSeaLevel">Pressure</SelectItem>
             <SelectItem value="dewPoint">Dew Point</SelectItem>
             <SelectItem value="windSpeed">Wind Speed</SelectItem>
             <SelectItem value="windGust">Wind Gust</SelectItem>
@@ -72,8 +86,8 @@ const ChartContainer = ({ weather }: Props) => {
         {chartData && (
           <LineChart
             data={chartData as Serie[]}
-            min={minMaxY.min - 10}
-            max={minMaxY.max + 10}
+            min={minMaxY.min - sliderValue[0]}
+            max={minMaxY.max + sliderValue[0]}
             fieldName={selectValue}
             fieldUnit={fieldUnit}
           />
