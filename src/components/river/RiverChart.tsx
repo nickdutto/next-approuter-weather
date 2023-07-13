4575e8df4b9b9a6a179215465c59db9e9a8e116f
Nick <delta2888@gmail.com>
2023-07-13 16:58:59 +1000

    feat: update styling for RiverChart
    - Fixed overflowing chart element that wasn't visible

--- src/components/river/RiverChart.tsx
+++ src/components/river/RiverChart.tsx
@@ -36,30 +36,36 @@ const RiverChart = ({ title, chartId, fieldName, fieldUnit, riverData, minMaxY }
   }, [timeRange, riverData, chartId]);
 
   return (
-    <div className="h-[400px] bg-zinc-950">
+    <div className="bg-zinc-950">
       <h3 className="pt-2 text-center font-semibold text-blue-400">{title}</h3>
-      <Select defaultValue={timeRange} onValueChange={setTimeRange}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Time Range" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="30">30 Days</SelectItem>
-          <SelectItem value="20">20 Days</SelectItem>
-          <SelectItem value="14">14 Days</SelectItem>
-          <SelectItem value="7">7 Days</SelectItem>
-          <SelectItem value="5">5 Days</SelectItem>
-        </SelectContent>
-      </Select>
-      {chartData && (
-        <LineChart
-          data={[chartData]}
-          fieldName={fieldName}
-          fieldUnit={fieldUnit}
-          min={yScaleMinMax.min}
-          max={yScaleMinMax.max}
-          tickSteps={24}
-        />
-      )}
+      <div className="flex justify-end pr-5 pt-2">
+        <div className="">
+          <Select defaultValue={timeRange} onValueChange={setTimeRange}>
+            <SelectTrigger className="w-[120px]">
+              <SelectValue placeholder="Time Range" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="30">30 Days</SelectItem>
+              <SelectItem value="20">20 Days</SelectItem>
+              <SelectItem value="14">14 Days</SelectItem>
+              <SelectItem value="7">7 Days</SelectItem>
+              <SelectItem value="5">5 Days</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="h-[400px]">
+        {chartData && (
+          <LineChart
+            data={[chartData]}
+            fieldName={fieldName}
+            fieldUnit={fieldUnit}
+            min={yScaleMinMax.min}
+            max={yScaleMinMax.max}
+            tickSteps={24}
+          />
+        )}
+      </div>
     </div>
   );
 };
