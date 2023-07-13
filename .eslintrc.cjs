51231413864076369d9d7ea9ad239a527dccc0f4
Nick <delta2888@gmail.com>
2023-07-13 14:50:13 +1000

    build: add typescript & import eslint plugins

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.{js,cjs}'],
+    },
+  ],
+  root: true,
+  rules: {
+    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+    ],
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+  },
+};
