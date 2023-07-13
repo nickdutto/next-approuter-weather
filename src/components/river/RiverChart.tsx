8d0fcd5ce926b228a79504b48b19396e634fa5d0
Nick <delta2888@gmail.com>
2023-07-13 16:39:45 +1000

    feat: add RiverChart component
    - Container for LineChart with its required logic

---
+++ src/components/river/RiverChart.tsx
@@ -0,0 +1,63 @@
+import { type Serie } from '@nivo/line';
+
+import { useEffect, useState } from 'react';
+
+import LineChart from '~/components/chart/LineChart';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { type RiverData } from '~/types/types';
+import { createRiverChartData, getRiverMinMaxValues } from '~/utils/river-utils';
+
+interface Props {
+  title: string;
+  chartId: string;
+  fieldName: string;
+  fieldUnit: string;
+  riverData: RiverData;
+}
+
+const RiverChart = ({ title, chartId, fieldName, fieldUnit, riverData }: Props) => {
+  const [chartData, setChartData] = useState<Serie | null>(null);
+  const [timeRange, setTimeRange] = useState('30');
+
+  const yScaleMinMax = getRiverMinMaxValues(riverData, { defaultMin: 1 });
+
+  useEffect(() => {
+    setChartData(createRiverChartData(riverData, chartId, parseInt(timeRange)));
+  }, [timeRange, riverData, chartId]);
+
+  return (
+    <div className="h-[400px] bg-zinc-950">
+      <h3 className="pt-2 text-center font-semibold text-blue-400">{title}</h3>
+      <Select defaultValue={timeRange} onValueChange={setTimeRange}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Time Range" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="30">30 Days</SelectItem>
+          <SelectItem value="20">20 Days</SelectItem>
+          <SelectItem value="14">14 Days</SelectItem>
+          <SelectItem value="7">7 Days</SelectItem>
+          <SelectItem value="5">5 Days</SelectItem>
+        </SelectContent>
+      </Select>
+      {chartData && (
+        <LineChart
+          data={[chartData]}
+          fieldName={fieldName}
+          fieldUnit={fieldUnit}
+          min={yScaleMinMax.min}
+          max={yScaleMinMax.max}
+          tickSteps={24}
+        />
+      )}
+    </div>
+  );
+};
+
+export default RiverChart;
--- src/components/river/RiverContainer.tsx
+++ src/components/river/RiverContainer.tsx
@@ -1,20 +1,9 @@
 'use client';
 
-import { type Serie } from '@nivo/line';
-
-import { useEffect, useState } from 'react';
-
-import LineChart from '~/components/chart/LineChart';
+import RiverChart from '~/components/river/RiverChart';
 import RiverTable from '~/components/river/RiverTable';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '~/components/ui/select';
 import { type RiverData } from '~/types/types';
-import { createRiverChartData, getRiverMinMaxValues, mergeRiverData } from '~/utils/river-utils';
+import { mergeRiverData } from '~/utils/river-utils';
 
 interface Props {
   riverData: {
@@ -24,84 +13,25 @@ interface Props {
 }
 
 const RiverContainer = ({ riverData }: Props) => {
-  const [dischargeTimeRange, setDischargeTimeRange] = useState('30');
-  const [levelTimeRange, setLevelTimeRange] = useState('30');
-  const [dischargeData, setDischargeData] = useState<Serie | null>(null);
-  const [levelData, setLevelData] = useState<Serie | null>(null);
-
   const mergedRiverData = mergeRiverData(riverData.discharge[0], riverData.level[0]);
-  const dischargeMinMaxY = getRiverMinMaxValues(riverData.discharge[0], { defaultMin: 1 });
-  const levelMinMaxY = getRiverMinMaxValues(riverData.level[0], { defaultMin: 1, defaultMax: 2 });
-
-  useEffect(() => {
-    setDischargeData(
-      createRiverChartData(
-        riverData.discharge[0],
-        'Watercourse Discharge',
-        parseInt(dischargeTimeRange),
-      ),
-    );
-  }, [dischargeTimeRange, riverData.discharge]);
-
-  useEffect(() => {
-    setLevelData(createRiverChartData(riverData.level[0], 'Water Level', parseInt(levelTimeRange)));
-  }, [levelTimeRange, riverData.level]);
 
   return (
     <div className="flex w-full flex-col gap-2">
       <div className="flex flex-col gap-2">
-        <div className="h-[400px] bg-zinc-950">
-          <h3 className="pt-2 text-center font-semibold text-blue-400">
-            Watercourse Discharge (cumec)
-          </h3>
-          <Select defaultValue={dischargeTimeRange} onValueChange={setDischargeTimeRange}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Time Range" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="30">30 Days</SelectItem>
-              <SelectItem value="20">20 Days</SelectItem>
-              <SelectItem value="14">14 Days</SelectItem>
-              <SelectItem value="7">7 Days</SelectItem>
-              <SelectItem value="5">5 Days</SelectItem>
-            </SelectContent>
-          </Select>
-          {dischargeData && (
-            <LineChart
-              data={[dischargeData]}
-              fieldName="Watercourse Discharge"
-              fieldUnit="cumec"
-              min={dischargeMinMaxY.min}
-              max={dischargeMinMaxY.max}
-              tickSteps={24}
-            />
-          )}
-        </div>
-        <div className="h-[400px] bg-zinc-950">
-          <h3 className="pt-2 text-center font-semibold text-blue-400">River Level (m)</h3>
-          <Select defaultValue={levelTimeRange} onValueChange={setLevelTimeRange}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Time Range" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="30">30 Days</SelectItem>
-              <SelectItem value="20">20 Days</SelectItem>
-              <SelectItem value="14">14 Days</SelectItem>
-              <SelectItem value="7">7 Days</SelectItem>
-              <SelectItem value="5">5 Days</SelectItem>
-            </SelectContent>
-          </Select>
-          {levelData && (
-            <LineChart
-              data={[levelData]}
-              fieldName="Watercourse Level"
-              fieldUnit="m"
-              min={1}
-              max={levelMinMaxY.max}
-              tickSteps={24}
-            />
-          )}
-        </div>
+        <RiverChart
+          title="Watercourse Discharge (cumec)"
+          chartId="WatercourseDischarge"
+          fieldName="Watercourse Discharge"
+          fieldUnit="cumec"
+          riverData={riverData.discharge[0]}
+        />
+        <RiverChart
+          title="River Level (m)"
+          chartId="WaterLevel"
+          fieldName="Water Level"
+          fieldUnit="m"
+          riverData={riverData.level[0]}
+        />
       </div>
       <RiverTable riverData={mergedRiverData} />
     </div>
