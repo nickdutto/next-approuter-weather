755a1f2ee983bd0d0fbac497088c24d96a790b46
Nick <delta2888@gmail.com>
2023-08-07 16:19:56 +1000

    feat: move icons from <head> into metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 
+import { type Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import Footer from '~/components/layouts/Footer';
@@ -12,23 +13,18 @@ import { cn } from '~/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Next Weather',
   description: 'Canberra Weather App',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </head>
       <body className={cn(inter.className, 'bg-black')}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <div>
