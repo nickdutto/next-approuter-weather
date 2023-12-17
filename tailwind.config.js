b5f0f3500546977d4203e99c1e4ad424c49a9b61
Nick <delta2888@gmail.com>
2023-12-17 14:24:25 +1100

    feat: :lipstick: add mantine night colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,18 +20,18 @@ module.exports = {
     },
     extend: {
       colors: {
-        dark: {
-          50: '#27272a',
-          100: '#232326',
-          200: '#1f1f22',
-          300: '#1b1b1d',
-          400: '#171719',
-          500: '#141415',
-          600: '#101011',
-          700: '#0c0c0d',
-          800: '#080808',
-          900: '#040404',
-          950: '#000000',
+        'm-night': {
+          0: 'var(--mantine-color-night-0)',
+          1: 'var(--mantine-color-night-1)',
+          2: 'var(--mantine-color-night-2)',
+          3: 'var(--mantine-color-night-3)',
+          4: 'var(--mantine-color-night-4)',
+          5: 'var(--mantine-color-night-5)',
+          6: 'var(--mantine-color-night-6)',
+          7: 'var(--mantine-color-night-7)',
+          8: 'var(--mantine-color-night-8)',
+          9: 'var(--mantine-color-night-9)',
+          10: 'var(--mantine-color-night-10)',
         },
       },
       keyframes: {
