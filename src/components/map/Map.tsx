db516a643b461a266c5a48a907730f29777d14cb
Nick <delta2888@gmail.com>
2024-01-10 17:07:20 +1100

    feat: :sparkles: update initial map position & zoom

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -14,8 +14,8 @@ const Map = () => {
     <GlMap
       initialViewState={{
         longitude: 149.069,
-        latitude: -35.45,
-        zoom: 9,
+        latitude: -35.7,
+        zoom: 7,
       }}
       style={{ width: '100%', height: '100%' }}
       mapStyle={`https://api.maptiler.com/maps/backdrop-dark/style.json?key=${process.env.NEXT_PUBLIC_MAP_TILER_API_KEY}`}
