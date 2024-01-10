8fb6ef4f3039ca1968b1dd65a4a8711e702949ed
Nick <delta2888@gmail.com>
2024-01-10 17:05:13 +1100

    feat: :sparkles: update links & styling in Navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -7,14 +7,35 @@ import NavbarRiverMenu from '~/components/layouts/NavbarRiverMenu';
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between rounded-m-lg bg-m-night-7 px-4 py-2">
-      <div className="flex items-center gap-4">
-        <Title order={1} c="blue.7" className="text-m-md sm:text-m-xl">
+      <div className="flex items-center gap-1 sm:gap-2">
+        <Title order={1} c="blue.7" className="text-m-sm sm:text-m-lg">
           NextWeather
         </Title>
         <div className="flex">
           <NavbarRiverMenu />
-          <Button component={Link} href="/map" fw={600} variant="subtle" color="gray.5">
+          <Button
+            component={Link}
+            href="/map"
+            fw={600}
+            variant="subtle"
+            color="gray.5"
+            classNames={{
+              root: 'text-m-xs sm:text-m-sm',
+            }}
+          >
             Map
+          </Button>
+          <Button
+            component={Link}
+            href="/"
+            fw={600}
+            variant="subtle"
+            color="gray.5"
+            classNames={{
+              root: 'text-m-xs sm:text-m-sm',
+            }}
+          >
+            Weather
           </Button>
         </div>
       </div>
