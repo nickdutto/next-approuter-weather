84b2cc778892afdf10a126dd3c217dcc7a1b4c5d
Nick <delta2888@gmail.com>
2024-01-10 17:41:36 +1100

    feat: :sparkles: add controls to Map & add custom styling

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
+import '~/styles/maplibregl.css';
 
 import { useState } from 'react';
-import GlMap from 'react-map-gl/maplibre';
+import GlMap, { GeolocateControl, NavigationControl, ScaleControl } from 'react-map-gl/maplibre';
 
 import Popup, { type PopupInfo } from '~/components/map/Popup';
 import WaterDataMarkers from '~/components/map/WaterDataMarkers';
@@ -17,9 +18,14 @@ const Map = () => {
         latitude: -35.7,
         zoom: 7,
       }}
+      minPitch={0}
+      maxPitch={0}
       style={{ width: '100%', height: '100%' }}
       mapStyle={`https://api.maptiler.com/maps/backdrop-dark/style.json?key=${process.env.NEXT_PUBLIC_MAP_TILER_API_KEY}`}
     >
+      <GeolocateControl />
+      <NavigationControl />
+      <ScaleControl />
       <WaterDataMarkers setPopupInfo={setPopupInfo} />
       <Popup popupInfo={popupInfo} setPopupInfo={setPopupInfo} />
     </GlMap>
