bd0bb23423adb326a078f44d9e17efa90ba321e6
Nick <delta2888@gmail.com>
2024-01-11 11:41:01 +1100

    fix: :bug: wrap NavProgressProvider in Suspense to fix enitre page deopted-into-client-rendering on all routes
    See https://nextjs.org/docs/messages/deopted-into-client-rendering

--- src/components/providers/NavProgressProvider.tsx
+++ src/components/providers/NavProgressProvider.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { Next13ProgressBar } from 'next13-progressbar';
+import { Suspense } from 'react';
 
 const NavProgressProvider = () => {
   return (
-    <Next13ProgressBar
-      height="4px"
-      color="#1c7ed6"
-      options={{ showSpinner: false }}
-      showOnShallow
-    />
+    <Suspense>
+      <Next13ProgressBar
+        height="4px"
+        color="#1c7ed6"
+        options={{ showSpinner: false }}
+        showOnShallow
+      />
+    </Suspense>
   );
 };
 
