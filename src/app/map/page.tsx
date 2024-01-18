b45ad5ed8b588baa2b1d1d3c655f2d512fb1100d
Nick <delta2888@gmail.com>
2024-01-18 12:17:28 +1100

    fix: :bug: fix incorrect/no height being applied to / , /map & /windy pages

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,8 +2,8 @@ import Map from '~/components/map/Map';
 
 const Page = () => {
   return (
-    <main className="flex h-full min-h-[calc(100svh-84px)] w-full flex-col gap-2 pt-4">
-      <div className="h-full w-full overflow-hidden rounded-m-lg">
+    <main className="flex h-full w-full flex-col gap-2 pt-4">
+      <div className="h-[calc(100svh-100px)] w-full overflow-hidden rounded-m-lg">
         <Map />
       </div>
     </main>
